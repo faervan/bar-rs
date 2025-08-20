c7f0a583acb52581cf85ab849a36999e0ce5d44b
faervan <faervan@proton.me>
2025-08-20 14:33:14 +0200

    Remove unused `old` directory

--- old/modules/mod.rs
+++
@@ -1,9 +0,0 @@
-use std::{any::Any, fmt::Debug};
-
-use crate::subscription::Subscription;
-
-pub trait Module: Any + Debug {
-    fn subscription(&self) -> Subscription {
-        Subscription::none()
-    }
-}
